ebc14c174907ac32407d20041b892e3bc45921fc
Sumit Raj <toppersumitr@iitkgp.ac.in>
2021-06-08 22:10:52 +0530

    Rebuilding App commit

---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, {useEffect,useState} from 'react'
+import './Nav.css'
+
+function Nav() {
+  const [show,handleShow]=useState(false)
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      if(window.scrollY>100){
+        handleShow(true)
+      }else{
+        handleShow(false)
+      }
+    })
+
+    return ()=>{
+      window.removeEventListener("scroll")
+    }
+  },[])
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"/>
+
+      <img className="nav__avatar" src="https://www.svgrepo.com/show/158427/facebook.svg" alt ="Netlix Logo"/>
+    </div>
+  )
+}
+
+export default Nav
